f388dbc33fdd102b02403983dd31cbd729f25cab
Mleon195 <mleon@mail.ccsf.edu>
2016-04-26 22:41:13 -0700

    homework lab

--- starter_code/monster.rb
+++ starter_code/monster.rb
@@ -1,16 +1,26 @@
 # Monster class
+class Monster
   # getters and setters for instance variables
-
+  attr_accessor :threat_level, :habitat, :name
   # class variable for count
-
+  @@count = 0
   # class instance variable for class description
-
+  @class_description = "A scary monster!"
   # class method getter for @@count class variable
-
+  def self.count
+      @@count
+  end
   # class method getter for @class_description class instance variable
-
+  def self.class_description
+    @class_description
+  end
   # initial behavior
-
+  def initialize(threat_level= :medium)
+      @threat_level = threat_level
+      puts "Rawr!"
+      @@count = @@count + 1
+      puts "#{@@count} monsters now roam the world!"
+  end
   # habitat? instance method
 
   # get_dangerous instance method
@@ -20,7 +30,7 @@
 # Zombie class
 
   # zombie version of class_description
-  
+
   # initial behavior and values
 
 # Werewolf class
